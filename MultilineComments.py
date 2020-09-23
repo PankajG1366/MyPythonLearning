de372a0afd1b163aede0f1b769d90c1898b502d4
PankajG1366 <Abhuu@2019()!@>
2020-09-22 20:51:06 -0400

    Character Picture Grid

---
+++ MultilineComments.py
@@ -0,0 +1,18 @@
+'''This is a test Python program
+written by Pankaj Gautam
+
+This program is designed for Python3, not Python2.
+'''
+
+def spam():
+    '''This is a multiline comment to help
+    explain what the spam() function does.'''
+    print('''
+    Dear Alice,
+    
+    Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+    
+    Sincerely,
+    Bob''')
+
+spam()
